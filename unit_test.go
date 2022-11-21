570831ebe25268549b5140d0f18efe1e2e2f5f86
zen <zhangyiming748@gmail.com>
2022-11-21 23:33:14 +0800

    通过测试

--- unit_test.go
+++ unit_test.go
@@ -3,9 +3,9 @@ package GBK2UTF8
 import "testing"
 
 func TestGBK2UTF8(t *testing.T) {
-	src := ""
-	dst := ""
-	pattern := ""
+	src := "/Users/zen/Downloads/Telegram/Telegram"
+	dst := "/Users/zen/Downloads/Telegram/Telegram/utf8"
+	pattern := "txt"
 	GBK2UTF8(src, pattern, dst)
 
 }
